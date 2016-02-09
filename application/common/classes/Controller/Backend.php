ed8c33f29f5e8d44fa419a404f1649fa9c39afcb
Piotr Theis <piotr@piotr-System-Product-Name>
2016-02-09 15:39:34 +0100

    controller backend

--- application/common/classes/Controller/Backend.php
+++ application/common/classes/Controller/Backend.php
@@ -4,46 +4,24 @@ defined('SYSPATH') or die('No direct script access.');
 
 abstract class Controller_Backend extends Controller_Site
 {
-    /**
-     * SEO tags
-     * @var string
-     */
-    public $title;
-    public $keywords;
-    public $meta_title;
-    public $meta_description;
-    
-    
-    /**
-     * Open graph tags
-     * @var string
-     */
-    public $og_title;
-    public $og_image;
-    public $og_description;
-    
-    /**
-     * Twitter cards
-     * @var string
-     */
-    public $twitter_title;
-    public $twitter_image;
-    public $twitter_description;
-    
-    
-    /**
-     * Css, js wrapper
-     * @var \Assets
-     */
-    protected $_assets;
-    
-    
-    /**
-     * Page breadcrumbs
-     * @var \Breadcrumbs
-     */
-    protected $_breadcrumbs;
 
+    /**
+     * Backend module site name
+     * @var string
+     */
+    protected $_mod_name;
+    
+    /**
+     * Backend module primary model
+     * @var \Jelly_Model
+     */
+    protected $_mod_model;
+    
+    /**
+     * Backend model meta object
+     * @var \Jelly_Meta
+     */
+    protected $_mod_model_meta;
 
 
     public function before()
@@ -57,19 +35,7 @@ abstract class Controller_Backend extends Controller_Site
 
     public function after()
     {
-        View::set_global('title', $this->title);
-        View::set_global('keywords', $this->keywords);
-        View::set_global('meta_title', $this->meta_title);
-        View::set_global('meta_description', $this->meta_description);
-        View::set_global('og_title', $this->og_title);
-        View::set_global('og_image', $this->og_image);
-        View::set_global('og_description', $this->og_description);
-        View::set_global('twitter_title', $this->twitter_title);
-        View::set_global('twitter_image', $this->twitter_image);
-        View::set_global('twitter_description', $this->twitter_description);
-        View::set_global('asets', $this->_assets->render());
-        View::set_global('breadcrumbs', $this->_breadcrumbs);
-        
+
         parent::after();
     }
 
